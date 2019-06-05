154015fd37a72340fc1903f7d002bdf6fc2b53b0
ryanboris <ryan.boris@gmail.com>
2019-06-05 00:14:53 -0400

    final push of the night

--- index.js
+++ index.js
@@ -22,3 +22,63 @@
 // aVariable
 // console.log(typeof aVariable)
 // //initialize
+
+//console.log(b)
+
+// var aVar
+// aVar = 'hi'
+
+// console.log(aVar)
+
+// var aVar = 'hi'
+// var anotherVar = null
+
+//not defined
+//undefined
+//null
+// 0
+// {}
+// let x = 3 //?
+// let y = 10 //?
+
+// x = 34 //?
+
+// y = 3498
+
+//argument
+
+// function myFunc(x) {
+//  x = x + 1
+//  return x
+// }
+
+// function myOther() {
+//   return 3
+// }
+
+// const x = 10000
+
+// console.log(myFunc(1))
+// console.log(myFunc(4))
+// console.log(myFunc(100))
+// console.log(myFunc(x))
+// console.log(myFunc(myOther()))
+
+const arr = [1, 2, 3, 4]
+
+const x = [0, 400, 5, 250, 22]
+x[0] //?
+x[1] //?
+
+const obj = { breakfast: 'muffin' }
+obj['breakfast'] //?
+
+const meals = {
+  breakfast: 'muffin',
+  lunch: 'skipped',
+  supper: 'cereal'
+}
+
+arr.map(number => number * 2) //?
+arr.filter(number => number % 2 === 0) //?
+arr.reduce((x, y) => x + y) //?
