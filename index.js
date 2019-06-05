3c78a40a41a3bc997c16359224601e91a0d4554f
ryanboris <ryan.boris@gmail.com>
2019-06-04 22:17:06 -0400

    adds var explanations

---
+++ index.js
@@ -0,0 +1,24 @@
+//1st pass - keywords (var, let, const, function) identifies and creates a reference in memory
+//2nd pass - assign by value - data to each of these keywords
+
+//String*
+//Number*
+//Boolean(Bool)*
+//Null
+//Objects
+//Arrays
+//Functions
+//NaN
+
+// console.log(typeof 'hey')
+
+// //variable
+// //var* - avoid this
+
+// //let, const
+
+// var aVariable //declared
+// aVariable = 12
+// aVariable
+// console.log(typeof aVariable)
+// //initialize
